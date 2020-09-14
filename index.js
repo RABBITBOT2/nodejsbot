c7acc14e05799fbbd2a9521682cedc301484ed4b
RABBITBOT2 <rabbitjsbot@gmail.com>
2020-09-14 11:39:59 +0900

    내정보

--- index.js
+++ index.js
@@ -140,10 +140,29 @@ client.on('message', (message) => {
           .addField('**한강물의 온도**', '22.1℃', true)
           .setColor('#0011ff')
           .setFooter('2020년 09월 06일 기준입니다', img)
-
-
-
       message.channel.send(embed)
+
+    } else if(message.content == '!내정보') {
+      let img = 'https://cdn.discordapp.com/avatars/733149844453195889/d29d770374b576cf541e3b0e5ea3abc3.png?size=128';
+      let embed = new Discord.RichEmbed()
+      let user = message.mentions.users.first() || message.author;
+      const joinDiscord = moment(user.createdAt).format('llll');
+      const joinServer = moment(user.joinedAt).format('llll');
+      let embed = new Discord.RichEmbed()
+          .setAuthor(user.username + '#' + user.discriminator, user.displayAvatarURL)
+          .setDescription(`${user}`)
+          .setColor(`RANDOM`)
+          .setThumbnail(`${user.displayAvatarURL}`)
+          .addField('Joined at:', `${moment.utc(user.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
+          .addField('Status:', user.presence.status, true)
+          .addField('Roles:', user.roles.map(r => `${r}`).join(' | '), true)
+          .setFooter(`ID: ${user.id}`)
+          .setTimestamp();
+    
+          message.channel.send(embed)
+    
+
+
 
               } else if(message.content == '!코로나') {
                   let helpImg = 'https://cdn.discordapp.com/avatars/733149844453195889/d29d770374b576cf541e3b0e5ea3abc3.png?size=128';
@@ -424,105 +443,6 @@ async function AutoMsgDelete(message, str, delay = 3000) {
     msg.delete();
   }, delay);
 }
-/////
-module.exports = {
-  name: "userinfo",
-  aliases: ["whois", "user"],
-  usage: "userinfo <MENTION>",
-  description: "Get advance stats of given person or yourself",
-  run: async (client, message, args) => {
-
-
-    let user;
-
-    if (!args[0]) {
-      user = message.member;
-    } else {
-
-
-      if (isNaN(args[0])) return message.channel.send(":x: Invalid ID of the user.")
-
-
-      user = message.mentions.members.first() || await message.guild.members.fetch(args[0]).catch(err => { return message.channel.send(":x: Unable to find this Person") })
-    }
-
-    if (!user) {
-      return message.channel.send(":x: Unable to find this person!")
-    }
-
-
-    //OPTIONS FOR STATUS
-
-    let stat = {
-      online: "https://emoji.gg/assets/emoji/9166_online.png",
-      idle: "https://emoji.gg/assets/emoji/3929_idle.png",
-      dnd: "https://emoji.gg/assets/emoji/2531_dnd.png",
-      offline: "https://emoji.gg/assets/emoji/7445_status_offline.png"
-    }
-
-    //NOW BADGES
-    let badges = await user.user.flags
-    badges = await badges.toArray();
-
-    let newbadges = [];
-    badges.forEach(m => {
-      newbadges.push(m.replace("_", " "))
-    })
-
-    let embed = new MessageEmbed()
-      .setThumbnail(user.user.displayAvatarURL({ dynamic: true }))
-
-    //ACTIVITY
-    let array = []
-    if (user.user.presence.activities.length) {
-
-      let data = user.user.presence.activities;
-
-      for (let i = 0; i < data.length; i++) {
-        let name = data[i].name || "None"
-        let xname = data[i].details || "None"
-        let zname = data[i].state || "None"
-        let type = data[i].type
-
-        array.push(`**${type}** : \`${name} : ${xname} : ${zname}\``)
-
-        if (data[i].name === "Spotify") {
-          embed.setThumbnail(`https://i.scdn.co/image/${data[i].assets.largeImage.replace("spotify:", "")}`)
-        }
-
-        embed.setDescription(array.join("\n"))
-
-      }
-    }
-
-      //EMBED COLOR BASED ON member
-      embed.setColor(user.displayHexColor === "#000000" ? "#ffffff" : user.displayHexColor)
-
-      //OTHER STUFF 
-      embed.setAuthor(user.user.tag, user.user.displayAvatarURL({ dynamic: true }))
-
-      //CHECK IF USER HAVE NICKNAME
-      if (user.nickname !== null) embed.addField("Nickname", user.nickname)
-      embed.addField("Joined At", moment(user.user.joinedAt).format("LLLL"))
-        .addField("Account Created At", moment(user.user.createdAt).format("LLLL"))
-        .addField("Common Information", `ID: \`${user.user.id}\`\nDiscriminator: ${user.user.discriminator}\nBot: ${user.user.bot}\nDeleted User: ${user.deleted}`)
-        .addField("Badges", newbadges.join(", ").toLowerCase() || "None")
-        .setFooter(user.user.presence.status, stat[user.user.presence.status])
-
-
-
-      return message.channel.send(embed).catch(err => {
-        return message.channel.send("Error : " + err)
-      })
-
-
-
-    }
-
-
-
-  }
-//////
 
 
 client.login(token);
