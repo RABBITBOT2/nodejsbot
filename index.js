74bc900a7bb3670870b0db1c039c859fbc876584
RABBITBOT2 <rabbitjsbot@gmail.com>
2020-09-14 12:31:28 +0900

    수정

--- index.js
+++ index.js
@@ -141,26 +141,7 @@ client.on('message', (message) => {
           .setColor('#0011ff')
           .setFooter('2020년 09월 06일 기준입니다', img)
       message.channel.send(embed)
-    } else if(message.content == '!내정보') {
-      const embed = new Discord.RichEmbed()
-          .setColor("RANDOM")
-          .setThumbnail(message.author.avatarURL)
-          .addField(`${user.tag}`, `${user}`, true)
-          .addField("ID:", `${user.id}`, true)
-          .addField("Nickname:", `${member.nickname !== null ? `${member.nickname}` : 'None'}`, true)
-          .addField("Status:", `${user.presence.status}`, true)
-          .addField("In Server", message.guild.name, true)
-          .addField("Game:", `${user.presence.game ? user.presence.game.name : 'None'}`, true)
-          .addField("Bot:", `${user.bot}`, true)
-          .addField("Joined The Server On:", `${moment.utc(member.joinedAt).format("dddd, MMMM Do YYYY")}`, true)
-          .addField("Account Created On:", `${moment.utc(user.createdAt).format("dddd, MMMM Do YYYY")}`, true) 
-          .addField("Roles:", member.roles.map(roles => `${roles}`).join(', '), true)
-          .setFooter(`Replying to ${message.author.username}#${message.author.discriminator}`)
-      message.channel.send(embed)
-    
-
-
-
+      
               } else if(message.content == '!코로나') {
                   let helpImg = 'https://cdn.discordapp.com/avatars/733149844453195889/d29d770374b576cf541e3b0e5ea3abc3.png?size=128';
                   let commandList = [
