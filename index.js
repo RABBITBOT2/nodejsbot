2285e4d5800e75ce664bb6b77c0cf7235f794720
RABBITBOT2 <rabbitjsbot@gmail.com>
2020-09-20 17:44:03 +0900

    ㅇㅁㅈㄻㅈㄹㅈㅁ

--- index.js
+++ index.js
@@ -137,12 +137,10 @@ client.on('message', (message) => {
       message.channel.send(embed)
 
 
-lient.on("message", message => {
-  let msg = message.content.toUpperCase();
+client.on("message", message => {
   let cont = message.content.split(" ");
   let args = cont.slice(1);
-
-  if (msg.startsWith('!날씨')) {
+if(message.content == '!날씨') {
 
     weather.find({search: args.join(" "), degreeType: 'C'}, function(err, result) {
       if (err) message.channel.send(err);
