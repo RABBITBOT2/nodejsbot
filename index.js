dbc932fd33779a6db05954cfdeea7440881c9796
RABBITBOT2 <rabbitjsbot@gmail.com>
2020-09-22 20:50:41 +0900

    1

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ client.on("guildMemberRemove", (member) => {
   const deleteUser = member.user;
   const byeChannel = guild.channels.find(channel => channel.name == byeChannelName);
 
-  byeChannel.send(`*${deleteUser.id}* ${byeChannelComment}\n`);
+  byeChannel.send(`*${deleteUser.name}* ${byeChannelComment}\n`);
 });
 
 client.on('message', (message) => {
